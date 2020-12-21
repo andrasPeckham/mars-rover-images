2bca868683cdb8a46cd7b31eec367e79f72713c9
peckhama <peckham.andras@wysio.hu>
2020-12-22 00:07:53 +0100

    created menu and landingpage titles

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -11,14 +11,14 @@ export class HomeComponent implements OnInit {
   marsImageCenter;
   overSvg: boolean;
   marsCircles = [
-    {dashArray: this.getDashArrayWithGap(5, 10, 1000, 70), radius: '43vh', strokeWidth: '5', forwards: true},
-    {dashArray: '3, 10, 3, 10, 3, 10, 3, 10, 3, 10, 3, 10 3, 10, 3, 10, 3, 10, 3, 10, 300, 1000', radius: '48vh', strokeWidth: '3', forwards: false},
+    {dashArray: this.getDashArrayWithGap(5, 10, 1000, 70), radius: '45vh', strokeWidth: '5', forwards: true},
+    {dashArray: '3, 10, 3, 10, 3, 10, 3, 10, 3, 10, 3, 10 3, 10, 3, 10, 3, 10, 3, 10, 300, 1000', radius: '50vh', strokeWidth: '3', forwards: false},
     {dashArray: '600, 600', radius: '55vh', strokeWidth: '5', forwards: true},
-    {dashArray: '10, 15, 10, 15, 10, 15, 700, 1000', radius: '60vh', strokeWidth: '7', forwards: false},
+    {dashArray: '10, 15, 10, 15, 10, 15, 700, 1000', radius: '60vh', strokeWidth: '6', forwards: false},
     {dashArray: '5, 20, 10, 20, 30, 20, 50, 20, 30, 20, 10, 20, 5, 500', radius: '65vh', strokeWidth: '7', forwards: true},
-    {dashArray: this.getDashArrayWithGap(15, 30, 1000, 50), radius: '75vh', strokeWidth: '8', forwards: false},
-    {dashArray: '5, 15, 5, 15, 5, 15, 5, 15, 100, 50, 100, 50, 100, 400', radius: '90vh', strokeWidth: '9', forwards: true},
-    {dashArray: '600, 600', radius: '110vh', strokeWidth: '10', forwards: false}
+    {dashArray: this.getDashArrayWithGap(15, 30, 1000, 50), radius: '75vh', strokeWidth: '7', forwards: false},
+    {dashArray: '5, 15, 5, 15, 5, 15, 5, 15, 100, 50, 100, 50, 100, 400', radius: '90vh', strokeWidth: '8', forwards: true},
+    {dashArray: '600, 600', radius: '110vh', strokeWidth: '9', forwards: false}
   ];
   constructor() { }
 
