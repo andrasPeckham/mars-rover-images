19dcb5bcc1de7728926794ba2422315c9dd34e4a
peckhama <56548222+andrasPeckham@users.noreply.github.com>
2021-05-09 18:55:37 +0200

    Update mars-rover.ts

--- src/app/models/RoverResult/mars-rover.ts
+++ src/app/models/RoverResult/mars-rover.ts
@@ -4,15 +4,10 @@ export class MarsRover {
   cameras: Camera[];
   id: number;
   name: string;
-  // tslint:disable-next-line:variable-name
   landing_date: Date;
-  // tslint:disable-next-line:variable-name
   launch_date: Date;
   status: string;
-  // tslint:disable-next-line:variable-name
   max_sol: number;
-  // tslint:disable-next-line:variable-name
   max_date: Date;
-  // tslint:disable-next-line:variable-name
   total_photos: number;
 }
